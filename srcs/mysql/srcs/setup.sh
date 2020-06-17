fecfd51aef61d8220a6f45707a79344b171bf2ee
AdrianWR <adrian.w.roque@gmail.com>
2020-06-17 11:31:23 -0300

    Add MySQL database pod configuration

--- srcs/mysql/srcs/setup.sh
+++ srcs/mysql/srcs/setup.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
-MYSQL_DATA_DIR=/data
-export TELEGRAF_CONFIG_PATH=/etc/telegraf.conf
-
-if [ ! -d $MYSQL_DATA_DIR/mysql ]
-then
-	echo "instalando"
-	/usr/bin/mysql_install_db --user=mysql --datadir=$MYSQL_DATA_DIR
-	chown mysql:mysql $MYSQL_DATA_DIR
-	/usr/bin/mysqld --user=root --bootstrap --verbose=0 < /tmp/mysql.sql
+if [[ ! -d /var/run/mysqld ]]; then
+	mkdir -p /var/run/mysqld
+	chown -R mysql:mysql /var/run/mysqld
 fi
 
-telegraf & exec /usr/bin/mysqld --user=root --console
+if [[ ! -d /data/mysql ]]; then
+	mysql_install_db --user=root --datadir=/data
+	
+	if [[ $MYSQL_ROOT_PASSWORD == "" ]]; then
+		MYSQL_ROOT_PASSWORD = "admin"
+	fi
+	
+	tfile=`mktemp`
+	cat > $tfile << EOF
+USE mysql;
+FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '';
+EOF
+	
+	/usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tfile
+	rm -f $tfile
+fi
+
+exec /usr/bin/mysqld --user=root --console
