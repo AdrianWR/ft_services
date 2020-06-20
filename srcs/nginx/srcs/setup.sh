e6a324699dcbe4b59ca3fc5a993e10be7751d9c1
AdrianWR <adrian.w.roque@gmail.com>
2020-06-20 19:53:26 -0300

    Update reverse proxy server config

--- srcs/nginx/srcs/setup.sh
+++ srcs/nginx/srcs/setup.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+SSH_USER=admin
+SSH_PASS=admin
+
+mkdir -p /var/run/nginx
+
 ssh-keygen -A
 adduser --disabled-password ${SSH_USER}
 echo "${SSH_USER}:${SSH_PASS}" | chpasswd
 /usr/sbin/sshd
 
 export TELEGRAF_CONFIG_PATH=/etc/telegraf.conf
-telegraf & nginx -g "pid /tmp/nginx.pid; daemon off;"
+#telegraf & nginx -g "pid /tmp/nginx.pid; daemon off;"
+nginx -g "daemon off;"
+#tail -f /dev/null
