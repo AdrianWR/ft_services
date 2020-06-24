d0c2bf2e7f5bee213927df9d5b8e1d5129e16ccd
AdrianWR <adrian.w.roque@gmail.com>
2020-06-24 15:24:00 -0300

    Add grafana reverse proxy path

--- srcs/nginx/srcs/setup.sh
+++ srcs/nginx/srcs/setup.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-SSH_USER=admin
-SSH_PASS=admin
+#SSH_USER=admin
+#SSH_PASS=admin
 
 mkdir -p /var/run/nginx
 
 ssh-keygen -A
-adduser --disabled-password ${SSH_USER}
-echo "${SSH_USER}:${SSH_PASS}" | chpasswd
+adduser --disabled-password ${SSH_USERNAME}
+echo "${SSH_USERNAME}:${SSH_PASSWORD}" | chpasswd
 /usr/sbin/sshd
 
-export TELEGRAF_CONFIG_PATH=/etc/telegraf.conf
-#telegraf & nginx -g "pid /tmp/nginx.pid; daemon off;"
 nginx -g "daemon off;"
-#tail -f /dev/null
