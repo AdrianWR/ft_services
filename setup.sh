922bf3e96137b83c45ec9fdb1de2d73742f569fc
Adrian W. Roque <adrian.w.roque@gmail.com>
2020-10-28 21:21:06 -0300

    Add start feature to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,12 @@
 PODS=(	
 		nginx		\
    		mysql		\
-		wordpress
+		phpmyadmin	\
+		wordpress	\
+		influxdb	\
+		grafana		\
+		nginx		\
+		ftps
 	 )
 
 function deploy()
@@ -22,64 +27,13 @@ function deploy()
 	return 0
 }
 
-if [[ "$1" == "restart" ]]; then
+minikube status
+if [[ $? == 85 || "$1" == "restart" ]]; then
 
-	#------------------- docker ---------------------#
+	if [[ "$1" == "restart" ]]; then
+		minikube delete
+	fi
 
-	#if [[ ! $(which docker) == "" ]]
-	#then
-	#	echo -e "${GREEN}Docker found...${NC}";
-	#else
-	#	echo -e "${RED}You need Docker to run this app.${NC}";
-	#	exit 1;
-	#fi
-
-	#docker ps > /dev/null;
-    #if [[ $? == 1 ]];
-    #then
-    #    printf "${RED}Docker doesn't have the right permissions${NC}";
-    #    sudo usermod -aG docker $USER;
-    #    printf "${GREEN}Permissions were applied. Please log out and in again...${NC}";
-    #    exit 1;
-
-    #fi
-	##----------------- kubernetes -------------------#
-
-	#if [ ! $(which kubectl) == "" ]
-	#then
-	#	echo -e "${GREEN}Kubernetes found...${NC}";
-	#else
-	#	echo -e "${GREEN}Downloading and installing Kubernetes...${NC}";
-	#	if [ ${SYSTEM_TYPE} == "Darwin" ]
-	#	then
-	#		curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl"
-	#	else
-	#		curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-	#	fi
-	#	chmod +x ./kubectl
-	#	sudo mv ./kubectl /usr/local/bin/kubectl
-	#fi
-	#kubectl version --client
-
-	##------------------ minikube --------------------#
-
-	#if [ ! $(which minikube) == "" ]
-	#then
-	#	echo -e "${GREEN}Minikube found...${NC}";
-	#else
-	#	curl -LO "https://storage.googleapis.com/minikube/releases/latest/minikube-${SYSTEM_TYPE}-amd64"
-	#	sudo install "minikube-${SYSTEM_TYPE}-amd64" /usr/local/bin/minikube
-	#fi
-	minikube delete
-
-	#----------- setup and permissions --------------#
-
-	#echo -e "${GREEN}Setting up permissions...${NC}"
-	#sudo systemctl enable docker.service
-	#sudo groupadd docker
-	#sudo usermod -aG docker $USER
-
-	#--------------- start minikube -----------------#
 	minikube start --driver=docker											\
 				   --bootstrapper=kubeadm										\
 				   --extra-config=kubelet.authentication-token-webhook=true		\
@@ -94,12 +48,15 @@ fi
 eval $(minikube docker-env)
 
 kubectl apply -f ./srcs/metallb/metallb.yaml
-deploy mysql
-deploy phpmyadmin
-deploy wordpress
-deploy influxdb
-deploy grafana
-deploy nginx
-deploy ftps
+for image in $PODS; do
+	deploy $image
+done
+#deploy mysql
+#deploy phpmyadmin
+#deploy wordpress
+#deploy influxdb
+#deploy grafana
+#deploy nginx
+#deploy ftps
 
-#minikube dashboard
+minikube dashboard
